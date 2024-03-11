42701718461c700174f1868e03c86ff29fdc7958
neizerjemima <113371239+neizerjemima@users.noreply.github.com>
2024-03-11 11:37:05 -0400

    useRef

--- JAY-REACT/src/App.js
+++ JAY-REACT/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
-import {useReducer} from "react";
+import {useRef} from "react";
 // const countries = ["Greece", "Mexico", "Philipines", "Maldives", "Costa Rica"];
 
 function App() {
-  const [country, nextCountry] = useReducer(country=>!country,false);
+  const textTitle = useRef();
+  const hexColour = useRef();
+  const submit = (e) => {e.preventDefault();
+    const title = textTitle.current.value;
+    const color = hexColour.current.value;
+    alert(`${title},${color}`);
+    textTitle.current.value = "";
+    hexColour.current.value="";
+  };
 
   return (
-    <div className="App">
-      <input type="checkbox" value={country} onChange={nextCountry} />
-      <label>{country ? "Maldives":"Home"}</label>
-    </div>
+    <form onSubmit={submit}>
+      <input ref={textTitle} type="text" placeholder="colour Title" />
+      <input ref={hexColour} type="color" />
+      <button>My Color</button>
+    </form>
   );
 }
 
