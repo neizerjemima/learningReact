06fd576b6d1aa0fdee8c370715f32f7b1c2cb6e8
neizerjemima <113371239+neizerjemima@users.noreply.github.com>
2024-03-11 11:20:11 -0400

    useReducer

--- JAY-REACT/src/App.js
+++ JAY-REACT/src/App.js
@@ -1,26 +1,14 @@
 import './App.css';
-import {useState,useEffect} from "react";
+import {useReducer} from "react";
 // const countries = ["Greece", "Mexico", "Philipines", "Maldives", "Costa Rica"];
 
 function App() {
-  const [country, nextCountry] = useState("Greece");
-  const [secondary, setSecondary] = useState("Home");
-
-  useEffect(()=> {
-    console.log (`Next location is ${country}`);
-  },[country]);
-
-  useEffect(()=> {
-    console.log (`Current location: ${secondary}`);
-  },[secondary]);
+  const [country, nextCountry] = useReducer(country=>!country,false);
 
   return (
     <div className="App">
-      <h1>I am on a flight to {country}</h1>
-      <button onClick={() => nextCountry("Maldives")}>Previous</button>
-      <button onClick={() => nextCountry("Costa Rica")}>Next</button>
-      <h2>Moving from {secondary}</h2>
-      <button onClick={() => setSecondary("Cafe")}>Current</button>
+      <input type="checkbox" value={country} onChange={nextCountry} />
+      <label>{country ? "Maldives":"Home"}</label>
     </div>
   );
 }
